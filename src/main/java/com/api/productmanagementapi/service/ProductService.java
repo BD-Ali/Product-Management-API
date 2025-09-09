0345b0e70270d0e1ac2e7f62c5146fd3ca94fefb
Ali Badarne <52676480+BD-Ali@users.noreply.github.com>
2025-09-09 13:05:12 -0700

    feat: implement ProductService interface

--- src/main/java/com/api/productmanagementapi/service/ProductService.java
+++ src/main/java/com/api/productmanagementapi/service/ProductService.java
@@ -11,3 +11,16 @@
 //    Product update(Long id, Product p);   // partial or full update
 //    void delete(Long id);
 //}
+
+package com.api.productmanagementapi.service;
+
+import com.api.productmanagementapi.entity.Product;
+import java.util.List;
+
+public interface ProductService {
+    List<Product> all();
+    Product get(Long id);
+    Product create(Product p);
+    Product update(Long id, Product p);
+    void delete(Long id);
+}
