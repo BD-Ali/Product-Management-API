34926050efb04890de01a2045e6aa3a1dfbdb41f
Ali Badarne <52676480+BD-Ali@users.noreply.github.com>
2025-09-09 13:04:09 -0700

    feat: implement ProductRepository interface

--- src/main/java/com/api/productmanagementapi/repository/ProductRepository.java
+++ src/main/java/com/api/productmanagementapi/repository/ProductRepository.java
@@ -6,3 +6,10 @@
 //public interface ProductRepository extends JpaRepository<Product, Long> {}
 //
 //
+package com.api.productmanagementapi.repository;
+
+import com.api.productmanagementapi.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+  }
