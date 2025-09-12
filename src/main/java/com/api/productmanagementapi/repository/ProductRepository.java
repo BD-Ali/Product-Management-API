ed3cadbf6dadf1ba03988dbd8378968b3cb2d1d9
BD-Ali <badarne3li@gmail.com>
2025-09-12 16:10:18 +0300

    added postgreSQL

--- src/main/java/com/api/productmanagementapi/repository/ProductRepository.java
+++ src/main/java/com/api/productmanagementapi/repository/ProductRepository.java
@@ -1,11 +1,3 @@
-//package com.api.productmanagementapi.repository;
-//
-//import com.api.productmanagementapi.entity.Product;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface ProductRepository extends JpaRepository<Product, Long> {}
-//
-//
 package com.api.productmanagementapi.repository;
 
 import com.api.productmanagementapi.entity.Product;
--- src/main/java/com/api/productmanagementapi/service/ProductService.java
+++ src/main/java/com/api/productmanagementapi/service/ProductService.java
@@ -1,20 +1,7 @@
-//package com.api.productmanagementapi.service;
-//
-//import com.api.productmanagementapi.entity.Product;
-//
-//import java.util.List;
-//
-//public interface ProductService {
-//    List<Product> all();
-//    Product get(Long id);
-//    Product create(Product p);
-//    Product update(Long id, Product p);   // partial or full update
-//    void delete(Long id);
-//}
-
 package com.api.productmanagementapi.service;
 
 import com.api.productmanagementapi.entity.Product;
+
 import java.util.List;
 
 public interface ProductService {
